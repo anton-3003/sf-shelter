45edcdfe61f993c31f7e265c3ed4f8f4d01ac575
anton-3003 <68605875+anton-3003@users.noreply.github.com>
2021-02-24 17:41:16 +0300

    Update settings.py

--- pet_shelter/settings.py
+++ pet_shelter/settings.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 import os
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+django_heroku.settings(locals())
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
